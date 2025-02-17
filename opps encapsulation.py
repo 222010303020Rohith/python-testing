e49cf39d118061e844e569220c96b2e376d7b738
222010303020Rohith <rkorukan@gitam.in>
2025-02-17 15:56:24 +0530

    protected method

--- opps encapsulation.py
+++ opps encapsulation.py
@@ -7,3 +7,17 @@ class PublicClass:
 
 obj = PublicClass()
 obj.display_name()  # This will print "Rohith"
+
+## protected method
+
+class Protected:
+    def __init__(self):
+        self._age = 30  # Protected attribute
+
+class Subclass(Protected):
+    def display_age(self):
+        print(self._age)  # Accessible in subclass
+
+obj = Subclass()
+obj.display_age()
+
