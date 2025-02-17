1430a6291f814de1baeb83a2bdb81a638c8f976b
222010303020Rohith <rkorukan@gitam.in>
2025-02-17 15:38:38 +0530

    opps encapsulation

---
+++ opps encapsulation.py
@@ -0,0 +1,9 @@
+class PublicClass:
+    def __init__(self):
+        self.name = "Rohith"
+
+    def display_name(self):
+        print(self.name)
+
+obj = PublicClass()
+obj.display_name()  # This will print "Rohith"
